b3f26615394557b414af07d4a53b461de96af686
Davide Filippi <9254428-Vadied@users.noreply.gitlab.com>
2023-02-04 13:11:11 +0100

    mini change

--- src/components/boardBlock/index.tsx
+++ src/components/boardBlock/index.tsx
@@ -6,7 +6,7 @@ interface Props extends IBlock {
 const BoardBlock = ({ value, handleClick }: Props) => {
   return (
     <div
-      className="w-32 h-32 border border-gray-400 text-center text-4xl font-bold"
+      className="flex justify-center items-center w-32 h-32 border border-gray-400 text-center text-4xl font-bold"
       onClick={handleClick}
     >
       {value !== "-" && value}
